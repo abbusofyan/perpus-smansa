91bb407ec1fbe104d0e937b3a2963bcd76027643
abbusofyan <sofyanutama1@gmail.com>
2020-12-05 01:39:00 +0700

    update login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 use Auth;
 
 class LoginController extends Controller
@@ -27,7 +28,8 @@ class LoginController extends Controller
      *
      * @var string
      */
-    // protected $redirectTo = RouteServiceProvider::HOME;
+
+    // protected $redirectTo = '/admin/';
 
     /**
      * Create a new controller instance.
@@ -50,4 +52,5 @@ class LoginController extends Controller
           return '/member/';
         }
     }
+
 }
